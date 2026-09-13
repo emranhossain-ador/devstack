d5c4e88d54023939db5f281b654832a80c2abdb5
emranhossain-ador <emranhossain.ador@gmail.com>
2026-09-13 13:07:18 +0600

    Fixed the response

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -14,11 +14,11 @@ const HeroSection = () => {
                     <p className="max-w-xl px-2 md:px-0">Explore frontend, backend, database, and tooling options, compare them side by side, and put together the stack that fits your next project.</p>
                 </div>
 
-                <div className="w-full flex items-center gap-4 px-4 md:px-0">
+                <div className="w-full flex items-center gap-2 md:gap-4 px-4 md:px-0">
 
-                    <button type="button" className="w-full whitespace-nowrap md:w-fit px-6 py-3 rounded-md gradient-btn text-white text-[15px] font-semibold tracking-wide shadow-[0_3px_6px] shadow-[#EC4899]/50 transition-all hover:shadow-none cursor-pointer">Explore Technologies</button>
+                    <button type="button" className="w-full  md:w-fit px-3 md:px-6 py-2.5 md:py-3 rounded-md gradient-btn text-white text-[12px] md:text-[15px] font-semibold md:tracking-wide shadow-[0_3px_6px] shadow-[#EC4899]/50 transition-all hover:shadow-none cursor-pointer">Explore Technologies</button>
 
-                    <button type="button" className="w-full whitespace-nowrap md:w-fit px-6 py-3 rounded-md border border-border bg-white text-foreground text-[15px] font-semibold tracking-wide transition-all hover:text-primary hover:border-primary cursor-pointer shadow">Learn More</button>
+                    <button type="button" className="w-full  md:w-fit px-3 md:px-6 py-2.5 md:py-3 rounded-md border border-border bg-white text-foreground text-[12px] md:text-[15px] font-semibold md:tracking-wide transition-all hover:text-primary hover:border-primary cursor-pointer shadow">Learn More</button>
 
                 </div>
             </div>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -9,10 +9,10 @@ const Navbar = () => {
         <div className="sticky top-0 z-50 w-full bg-white border-b border-border">
             <div className="container mx-auto w-full py-2.5 px-3 flex items-center justify-between">
                 
-                <button type="button" className="px-2.5 py-2 border border-gray-500/30 rounded-lg bg-gray-500/10 transition-all hover:scale-105 cursor-pointer inline md:hidden">
-                    <RiMenu2Line size={22} />
+                <button type="button" className="px-2 py-1.5 border border-gray-500/30 rounded-lg bg-gray-500/10 transition-all hover:scale-105 cursor-pointer inline md:hidden">
+                    <RiMenu2Line size={20} />
                 </button>
-                <img src={Logo} alt="Logo" className="h-auto md:h-11 w-auto" />
+                <img src={Logo} alt="Logo" className="h-6 md:h-11 w-auto" />
 
                 <nav className="md:inline hidden">
                     <ul className="flex items-center gap-3.5">
@@ -31,11 +31,11 @@ const Navbar = () => {
                     </ul>
                 </nav>
 
-                <div className="flex items-center gap-2 md:gap-4">
-                    <button type="button" className="px-4 py-2 tracking-wide flex items-center gap-2 whitespace-nowrap rounded-xl border border-primary/70 text-primary transition-all hover:bg-primary/10 font-sans font-semibold cursor-pointer">
+                <div className="flex items-center gap-1 md:gap-4">
+                    <button type="button" className="text-[13px] md:text-base px-4 py-1.5 md:py-2 md:tracking-wide flex items-center gap-2 whitespace-nowrap rounded-xl border border-primary/70 text-primary transition-all hover:bg-primary/10 font-sans font-semibold cursor-pointer">
                         <FiLogIn className="hidden md:inline" /> Sign In
                     </button>
-                    <button type="button" className="px-4 py-2 tracking-wide flex items-center gap-2 whitespace-nowrap rounded-xl border border-primary bg-primary text-white transition-all hover:bg-primary/90 font-sans font-semibold cursor-pointer">
+                    <button type="button" className="text-[13px] md:text-base px-4 py-1.5 md:py-2 md:tracking-wide flex items-center gap-2 whitespace-nowrap rounded-xl border border-primary bg-primary text-white transition-all hover:bg-primary/90 font-sans font-semibold cursor-pointer">
                         <FaUserPlus className="hidden md:inline" /> Sign Up
                     </button>
                 </div>
